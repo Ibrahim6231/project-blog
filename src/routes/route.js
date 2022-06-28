fbf25eb52995382343c27c5996d5c0d3112f729a
Ibrahim6231 <ibrahimkhan938@gmail.com>
2022-06-28 21:37:34 +0530

    route names changed only

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,16 +7,16 @@ const middleware = require('../middlewares/routeBasedMiddleware.js');
 const validator = require('../validator/validator.js')
 
 //new author registration
-router.post('/createAuthors', validator.validateAuthor, authorController.createAuthor);
+router.post('/authors', validator.validateAuthor, authorController.createAuthor);
 //login
-router.post('/authorLogin', authorController.login);
+router.post('/login', authorController.login);
 //create blogs, view blogs
-router.post('/createBlogs', middleware.checkLogin, validator.validateBlog, blogController.createBlog);
-router.get('/getBlogs', middleware.checkLogin, blogController.listBlogsByQuery);
+router.post('/blogs', middleware.checkLogin, validator.validateBlog, blogController.createBlog);
+router.get('/blogs', middleware.checkLogin, blogController.listBlogsByQuery);
 //edit blogs, delete blogs
-router.put('/updateBlog/:blogId', middleware.checkLogin, middleware.checkOwner, validator.validateToUpdate, blogController.updateBlog);
+router.put('/blogs/:blogId', middleware.checkLogin, middleware.checkOwner, validator.validateToUpdate, blogController.updateBlog);
 router.delete('/blogs/:blogId', middleware.checkLogin, middleware.checkOwner, blogController.deleteById);
-router.delete('/deleteBlogs/query', middleware.checkLogin, middleware.authForQueryDelete, blogController.deleteByQuery);
+router.delete('/blogs', middleware.checkLogin, middleware.authForQueryDelete, blogController.deleteByQuery);
 
 
  
